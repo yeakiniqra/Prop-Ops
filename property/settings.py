42b07c2b4d1b246dafc37f1c0c0223cb827fdb90
yeakiniqra <21201128@uap-bd.edu>
2024-05-07 00:40:52 +0600

    settings changes

--- property/settings.py
+++ property/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m9ra^lav43s54zj4wvlt1w(!gk(y^4901unuz_=pgk%i2nq@8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
